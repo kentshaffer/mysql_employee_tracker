d195cc476071a3f15e8e22a16ded3d3201a3ca41
kent shaffer <kentshaffer@kents-MacBook-Pro.local>
2022-09-05 17:24:11 -0700

    started switch statements and prompt

--- index.js
+++ index.js
@@ -2,3 +2,56 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const mysql = require('mysql2');
 require('dotenv').config()
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  },
+);
+
+function openingPrompt() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'openingPrompt',
+      message: 'what would you like to do?',
+      choices: ['view all departments',
+        'view all roles',
+        'view all employees',
+        'add a department',
+        'add a role',
+        'add an employee',
+        'update an employee role',
+        'quit']
+    },
+  ])
+    .then((promptChoice) => {
+      switch (promptChoice.openingPrompt) {
+        case 'view all departments':
+          viewDepartments();
+          break;
+        case 'view all roles':
+          viewRoles();
+          break;
+        case 'view all employees':
+          viewEmployees();
+          break;
+        case 'add a department':
+          addDepartment();
+          break;
+        case 'add a role':
+          addRole();
+          break;
+        case 'add an employee':
+          addEmployee();
+          break;
+        case 'update an employee role':
+          updateEmployeeRole();
+          break;
+        default:
+          quit();
+      }
+    });
+}
