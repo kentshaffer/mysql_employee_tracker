709fe55275f4ec15a517ed6c0137e4bd31cddf04
kent shaffer <kentshaffer@kents-MacBook-Pro.local>
2022-09-06 15:59:17 -0700

    added pathsto display tables in terminal. added fuction calls to loop through app to prompt more actions after one is completed. added fuction to close sql connection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const mysql = require('mysql2');
+const cTable = require('console.table');
 require('dotenv').config()
 const db = mysql.createConnection(
   {
@@ -18,10 +19,10 @@ function openingPrompt() {
       name: 'openingPrompt',
       message: 'what would you like to do?',
       choices: ['view all departments',
-        'view all roles',
-        'view all employees',
         'add a department',
+        'view all roles',
         'add a role',
+        'view all employees',
         'add an employee',
         'update an employee role',
         'quit']
@@ -32,17 +33,17 @@ function openingPrompt() {
         case 'view all departments':
           viewDepartments();
           break;
-        case 'view all roles':
-          viewRoles();
-          break;
-        case 'view all employees':
-          viewEmployees();
-          break;
         case 'add a department':
           addDepartment();
           break;
+        case 'view all roles':
+          viewRoles();
+          break;
         case 'add a role':
           addRole();
+          break;
+        case 'view all employees':
+          viewEmployees();
           break;
         case 'add an employee':
           addEmployee();
@@ -56,6 +57,13 @@ function openingPrompt() {
     });
 }
 
+function viewDepartments() {
+  db.query('SELECT * FROM department', function (err, results) {
+    console.table(results);
+        
+    openingPrompt();
+  });
+}
 
 function addDepartment() {
   inquirer.prompt([
@@ -65,17 +73,26 @@ function addDepartment() {
       message: 'What is the name of the department you would like to add?'
     },
   ]).then(answers => {
-    let newDepartment = {department_name: answers.departmentName}
+    let newDepartment = { department_name: answers.departmentName }
     db.promise().query('INSERT INTO department SET ?', newDepartment).then(dbData => console.log(dbData))
   })
 }
 
+function viewRoles() {
+  db.query('SELECT * FROM employee_role', function (err, results) {
+    console.table(results);
+        
+    openingPrompt();
+  });
+}
+
+
 async function addRole() {
   const [departments] = await db.promise().query('SELECT * FROM department')
-  const departmentArray = departments.map(({id, department_name}) => (
-  {
-    name: department_name, value: id
-  }
+  const departmentArray = departments.map(({ id, department_name }) => (
+    {
+      name: department_name, value: id
+    }
   ))
   inquirer.prompt([
     {
@@ -95,8 +112,10 @@ async function addRole() {
       choices: departmentArray
     },
   ]).then(answers => {
-    let roleObj = {title: answers.roleName, salary: answers.roleSalary, department_id: answers.existingDepartments}
+    let roleObj = { title: answers.roleName, salary: answers.roleSalary, department_id: answers.existingDepartments }
     db.promise().query('INSERT INTO employee_role SET ?', roleObj).then(dbData => console.log(dbData))
+        
+    openingPrompt();
   })
 }
 
@@ -105,13 +124,22 @@ async function addRole() {
 //fix the .then to let returned obj use prompt questions/existing table info
 //test 
 
+function viewEmployees() {
+  db.query('SELECT * FROM employee', function (err, results) {
+    console.table(results);
+
+    openingPrompt();
+  });
+}
+
+
 async function addEmployee() {
   const [roles] = await db.promise().query('SELECT * FROM employee_role')
-  const roleArray = roles.map(({title, role_title}) => (
-  {
-    name: role_title, value: title
+  const roleArray = roles.map(({ title, role_id }) => (
+    {
+      name: title, value: role_id
 
-  }
+    }
   ))
   inquirer.prompt([
     {
@@ -130,12 +158,33 @@ async function addEmployee() {
       message: 'What role does this employee hold?',
       choices: roleArray
     },
+    {
+      type: 'list',
+      name: 'possibleManager',
+      message: 'Who is the employees manager?',
+      choices: roleArray
+    },
   ]).then(answers => {
-    let employeeObj = {first_name: answers.firstName, last_name: answers.lastName, role_id: answers.existingRoles}
+    let employeeObj = { first_name: answers.firstName, last_name: answers.lastName, role_id: answers.existingRoles }
     db.promise().query('INSERT INTO employee SET ?', employeeObj).then(dbData => console.log(dbData))
+        
+    openingPrompt();
   })
 }
 
+// function quit() {
+//   db.query('quit')
+//     console.log('See Ya');
+//   };
+
+function quit() {   
+  db.end(function(err) {
+    if (err) {
+      return console.log('error:' + err.message);
+    }
+    console.log('Closed connection. See ya Later');
+  });
+}   
 
 
 
